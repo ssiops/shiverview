31f7d8c1e7ffe9f225649aa78a19768253eb68f5
akasfei <akasfei@gmail.com>
2014-09-15 19:47:20 -0700

    Refactored init sequence
    Fixed a bug where the server will not work if using clusters

--- server.js
+++ server.js
@@ -32,45 +32,46 @@ var RedisStore = require('connect-redis')(session);
 var Manager = require('./lib/manager.js');
 var error = require('./lib/errHandler.js');
 
+if (process.env.verbose) console.log('[%s]\nSystem started. Initializing system clusters.', t);
+
 init();
 
-if (cluster.isMaster) {
-  if (process.env.verbose) console.log('[%s]\nSystem started. Initializing system clusters.', t);
-  var server = module.exports = process.server = express();
+var server = module.exports = process.server = express();
 
-  server.locals.version = pkg.version;
-  if (pkg.production === true) {
-    server.set('env', 'production');
-  } else {
-    server.set('env', 'development');
-    server.use(logger('dev'));
-  }
-  server.use(compress());
-  server.use(bodyParser.json());
-  server.use(bodyParser.urlencoded({ extended: false }));
-  server.use(multiparty({limit: '8MB'}));
-  server.use(cookieParser());
-  server.use(session({
-    store: new RedisStore({client: redisClient}),
-    secret: 'shiverview',
-    saveUninitialized: true,
-    resave: true
-  }));
-  server.use(serveStatic(__dirname + '/static'));
-  server.use(serveStatic(__dirname + '/usercontent'));
-  server.use(methodOverride());
-  server.use(errorHandler({ dumpExceptions: true, showStack: true }));
-  server.set('view engine', 'html');
-  server.set('views', __dirname + '/views');
-  server.engine('html', require('hbs').__express);
+server.locals.version = pkg.version;
+if (pkg.production === true) {
+  server.set('env', 'production');
+} else {
+  server.set('env', 'development');
+  server.use(logger('dev'));
+}
+server.use(compress());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(multiparty({limit: '8MB'}));
+server.use(cookieParser());
+server.use(session({
+  store: new RedisStore({client: redisClient}),
+  secret: 'shiverview',
+  saveUninitialized: true,
+  resave: true
+}));
+server.use(serveStatic(__dirname + '/static'));
+server.use('/usercontent', serveStatic(__dirname + '/usercontent'));
+server.use(methodOverride());
+server.use(errorHandler({ dumpExceptions: true, showStack: true }));
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
+server.engine('html', require('hbs').__express);
 
-  var man = new Manager(server, pkg);
+var man = new Manager(server, pkg);
 
-  man.init(function(err) {
-    if (err) console.log(err);
-    server.emit('ready');
-    server.use(error.s404);
-    server.use(error.s500);
+man.init(function(err) {
+  if (err) console.log(err);
+  server.emit('ready');
+  server.use(error.s404);
+  server.use(error.s500);
+  if (cluster.isMaster) {
     if (process.env.single) {
       http.createServer(process.server).listen(process.env.port || 80, function () {
         console.log("HTTP server booted in %d ms on port 80.", new Date().getTime() - t.getTime());
@@ -85,15 +86,16 @@ if (cluster.isMaster) {
         cluster.fork();
       });
     }
-  });
-} else {
-  if (cluster.worker.id === 0 || typeof ssl === 'undefined')
-    http.createServer(process.server).listen(process.env.port || 80, function () {
-      console.log("HTTP server cluster #%d@%d booted in %d ms on port 80.", cluster.worker.id, cluster.worker.process.pid, new Date().getTime() - t.getTime());
-    });
-  if (typeof ssl !== 'undefined') {
-    https.createServer(ssl, process.server).listen(process.env.sslport || 443, function () {
-      console.log("HTTPS server cluster #%d@%d booted in %d ms on port 443.", cluster.worker.id, cluster.worker.process.pid, new Date().getTime() - t.getTime());
-    });
+  } else {
+    if (cluster.worker.id === 0 || typeof ssl === 'undefined')
+      http.createServer(process.server).listen(process.env.port || 80, function () {
+        console.log("HTTP server cluster #%d@%d booted in %d ms on port 80.", cluster.worker.id, cluster.worker.process.pid, new Date().getTime() - t.getTime());
+      });
+    if (typeof ssl !== 'undefined') {
+      https.createServer(ssl, process.server).listen(process.env.sslport || 443, function () {
+        console.log("HTTPS server cluster #%d@%d booted in %d ms on port 443.", cluster.worker.id, cluster.worker.process.pid, new Date().getTime() - t.getTime());
+      });
+    }
   }
-}
+
+});
